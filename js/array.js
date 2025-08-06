f99c41400eb3b193e5b87e74de4dd195bb4d53c5
Rodrigo Alexandre Vehman <rodvehman@hotmail.com>
2025-08-06 14:59:28 -0300

    Update

--- js/array.js
+++ js/array.js
@@ -6,3 +6,14 @@ const coisas = [10,"Senac","<h2>Olá</h2>", seila, 15.88];
 
 console.log(coisas[1]);
 console.log(coisas[coisas.length-1]);
+
+/* Exercícios
+
+1) Crie um array contendo o nome de 7 coisas que você gosta.
+
+2 Em seguida, mostre no console uma frase personalizada indicando o segundo, o quinto e o sétimo item do array. Use concatenação ou template strings.
+*/
+
+let itens = ["Deus", "Filmes", "Música", "Família", "Dinheiro", 1.78, "12"];
+
+console.log(`Segundo item: ${itens[1]}`+`\nQuinto item: ${itens[4]}`+`\nSétimo item: ${itens[6]}`);
