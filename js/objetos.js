166c3dc5c111cfe20d7cdb125d912b9a096131c4
Rodrigo Alexandre Vehman <rodvehman@hotmail.com>
2025-08-06 16:27:10 -0300

    First Version

---
+++ js/objetos.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//Exemplo 1: objeto com os dados de uma pessoa
+
+const pessoa = {
+    nome: "Chaves",
+    idade: 8,
+    cidade: "Acapulco",
+    estado: "ACA",
+};
+
+console.log(pessoa);
