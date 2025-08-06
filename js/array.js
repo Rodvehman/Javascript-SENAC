1412c4eafe764d02abb484666ed1dc6d0d1d3dfd
Rodrigo Alexandre Vehman <rodvehman@hotmail.com>
2025-08-06 14:33:43 -0300

    First Update

---
+++ js/array.js
@@ -0,0 +1,8 @@
+let seila = 6;
+
+// O JS aceita array com dados de tipos diferentes, conforme abaixo.
+
+const coisas = [10,"Senac","<h2>Olá</h2>", seila, 15.88];
+
+console.log(coisas[1]);
+console.log(coisas[coisas.length-1]);
