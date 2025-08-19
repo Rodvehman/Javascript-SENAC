97ca9ccd88682ed8c7f0e12188e0260a3fb32f66
Rodrigo Alexandre Vehman <rodvehman@hotmail.com>
2025-08-19 17:02:19 -0300

    Update

---
+++ js/operadoreslogicos.js
@@ -0,0 +1,37 @@
+'use strict'
+
+/* Operadores Lógicos
+    && -> E -> and;
+    || -> Ou -> or;
+    ! -> Negação -> not;
+    == -> Cracteres Iguais;
+    === -> caracteres e tipos de dados iguais.
+*/
+
+//Retorne True se todas as condições forem verdadeiras
+console.log("Exemplo &&:");
+let idade = 20;
+let temCarta = true;
+
+if (idade >=10 && temCarta){
+    console.log("Habilitado");
+} else {
+    console.log("Não Habilitado");    
+}
+
+if (idade >= 18 && temCarta === true){
+    console.log("Habilitado");
+    
+} else {
+    console.log("Não Habilitado");
+}
+
+ console.log("\nExemplo Ou - ||");
+let feriado = false;
+let fimDeSemana = true;
+
+if (feriado === true || fimDeSemana === true){
+    console.log("Não tem aula!");
+} else {
+    console.log("Tem aula!");    
+}
