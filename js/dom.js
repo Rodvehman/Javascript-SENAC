674905c59691a568b26690ce334af61323e58e61
Rodrigo Alexandre Vehman <rodvehman@hotmail.com>
2025-08-26 17:22:50 -0300

    Update

---
+++ js/dom.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* Exemplo 1 */
+
+const exemplo01 = document.querySelector("#exemplo1");
+const mensagem01 = document.querySelector("#mensagem01");
+const pagina = document.querySelector("body");
+
+console.log(exemplo01);
+console.log(mensagem01);
+console.log(pagina);
