367f24cf2eacaf0db2871ab4669d3612d1b0f192
Rodrigo Alexandre Vehman <rodvehman@hotmail.com>
2025-08-27 14:02:02 -0300

    Update

--- js/dom.js
+++ js/dom.js
@@ -2,10 +2,16 @@
 
 /* Exemplo 1 */
 
-const exemplo01 = document.querySelector("#exemplo1");
+const exemplo01 = document.getElementById("exemplo1");
 const mensagem01 = document.querySelector("#mensagem01");
 const pagina = document.querySelector("body");
 
 console.log(exemplo01);
 console.log(mensagem01);
 console.log(pagina);
+
+/* Para selecionar vários elementos, use querySelectorAll. O resultado é uma espécie de Array de elementos (NodeList) */
+
+const paragrafos = document.querySelectorAll("p");
+
+console.log(paragrafos);
